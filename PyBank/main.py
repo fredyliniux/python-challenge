fc24dec8dec1723ef33ca06f40cd832c4c3a962d
fredy <fredyreysa@gmail.com>
2021-06-19 18:35:09 -0700

    PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,14 +1,14 @@
 
-#Modules
 import os
 import csv
 
 #Set path for file
-csvpath = os.path.join('Resources', 'budget_data.csv')
+csvpath = os.path.join('Resurces', 'budget_data.csv')
 
 total_months = []
 net_profit = []
 profit_change = []
+
 H=lambda L:sum([L[x+1]-L[x] for x in range(len(L)-1)])/(len(L)-1)
 M=lambda L:max([L[x+1]-L[x] for x in range(len(L)-1)])
 N=lambda L:min([L[x+1]-L[x] for x in range(len(L)-1)])
@@ -18,45 +18,36 @@ with open("Resources/budget_data.csv", newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     header = next(csvreader)
 
-    #iterate values to add to list
+#iterate values to add to list
     for row in csvreader:
         total_months.append(row[0])
         net_profit.append(int(row[1]))
-    print(total_months)
+    #print(total_months)
     print()
+
+    for row in csvreader:
+        total_months.append(row[0])
+        net_profit.append(int(row[1]))
+
+    for i in range (len(net_profit)-1):
+        profit_change.append(net_profit[i+1]-net_profit[i])
     
-#print(len(total_months))
-print(sum(net_profit))
-print(H(net_profit))
-print(M(net_profit))
-print(N(net_profit))
+month_inc=profit_change.index(max(profit_change))+1
+month_dec=profit_change.index(min(profit_change))+1
 
-# print()
-# print("Financial Analysis")
-# print("-------------------")
-# print(f"Total Months:{len(total_months)}")
-# print(f"Total: ${sum(net_profit)}")
-# print(f"Average Change:{round(H(net_profit),2)}")
-# print(f"Greatest Increase in Profits: {total_months[monthly_increase]} (${(str(increase))})")
-# print(f"Greatest Decrease in Profits: {total_months[monthly_decrease]} (${(str(decrease))})")
+ans = []
 
-# # export text file of results
-# output = "output.txt"
-# with open("output","w") as new:
-#     new.write("Financial Analysis")
-#     new.write("\n")
-#     new.write("--------------------")
-#     new.write("\n")
-#     new.write(f"Total Months:{len(total_months)}")
-#     new.write("\n")
-#     new.write(f"Total: ${sum(net_profit)}")
-#     new.write("\n")
-#     new.write(f"Average Change: {round(sum(profit_change)/len(profit_change),2)}")
-#     new.write("\n")
-#     new.write(f"Greatest Increase in Profits: {total_months[monthly_increase]} (${(str(increase))})")
-#     new.write("\n")
-#     new.write(f"Greatest Decrease in Profits: {total_months[monthly_decrease]} (${(str(increase))})")
+ans.append("Financial Analysis\n-------------------------------")
+ans.append(f"Total Months:{len(total_months)}")
+ans.append(f"Total: ${sum(net_profit)}")
+ans.append(f"Average Change:{round(H(net_profit),2)}")
+ans.append(f"Greatest Increase in Profits: {total_months[month_inc]} (${(str(M(net_profit)))})")
+ans.append(f"Greatest Decrease in Profits: {total_months[month_dec]} (${(str(N(net_profit)))})")
 
+exp = 'Financial_Analysis.txt'
 
-
+with open(exp, 'w') as file:
+    for x in ans:
+        print(x)
+        file.write(x + '\n')
 
